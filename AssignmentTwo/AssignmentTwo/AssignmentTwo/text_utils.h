e6e418e6b5c28c6cd629507ea784786900bc3207
gkazimirov396 <grishakazim@gmail.com>
2024-06-11 17:29:27 +0300

    feat: Create header file

---
+++ AssignmentTwo/AssignmentTwo/AssignmentTwo/text_utils.h
@@ -0,0 +1,33 @@
+#pragma once
+
+class Line {
+public:
+    Line(const char* text = "");
+    ~Line();
+    const char* getText() const;
+    void appendText(const char* text);
+    void insertText(int charIndex, const char* text);
+
+    Line* next;
+
+private:
+    char* text;
+    void setText(const char* newText);
+};
+
+class TextEditor {
+public:
+    TextEditor();
+    ~TextEditor();
+    void appendText(const char* text);
+    void startNewLine();
+    void saveToFile(const char* filename) const;
+    void loadFromFile(const char* filename);
+    void printCurrentText() const;
+    void insertText(int lineNumber, int charIndex, const char* text);
+    void searchText(const char* query) const;
+    void clearText();
+
+private:
+    Line* head;
+};
