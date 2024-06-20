f97fcf02f830c601b73217944c1ea8e7ed2be008
gkazimirov396 <grishakazim@gmail.com>
2024-06-20 22:12:27 +0300

    feat: implement encrypt, decrypt functions, connect CMake

---
+++ AssignmentThree/AssignmentThree/caesar.cpp
@@ -0,0 +1,38 @@
+﻿#include "caesar.h"
+#include <cstring>
+#include <cctype>
+#include <cstdlib>
+
+char* encrypt(char* rawText, int key) {
+    int len = strlen(rawText);
+    char* encryptedText = (char*)malloc(len + 1);
+
+    for (int i = 0; i < len; i++) {
+        if (isalpha(rawText[i])) {
+            char offset = islower(rawText[i]) ? 'a' : 'A';
+            encryptedText[i] = ((rawText[i] - offset + key) % 26) + offset;
+        }
+        else {
+            encryptedText[i] = rawText[i];
+        }
+    }
+    encryptedText[len] = '\0';
+    return encryptedText;
+}
+
+char* decrypt(char* encryptedText, int key) {
+    int len = strlen(encryptedText);
+    char* decryptedText = (char*)malloc(len + 1);
+
+    for (int i = 0; i < len; i++) {
+        if (isalpha(encryptedText[i])) {
+            char offset = islower(encryptedText[i]) ? 'a' : 'A';
+            decryptedText[i] = ((encryptedText[i] - offset - key + 26) % 26) + offset;
+        }
+        else {
+            decryptedText[i] = encryptedText[i];
+        }
+    }
+    decryptedText[len] = '\0';
+    return decryptedText;
+}
---
+++ AssignmentThree/AssignmentThree/caesar.h
@@ -0,0 +1,15 @@
+﻿#ifndef CAESAR_H
+#define CAESAR_H
+
+#ifdef _WIN32
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT __attribute__((visibility("default")))
+#endif
+
+extern "C" {
+    EXPORT char* encrypt(char* rawText, int key);
+    EXPORT char* decrypt(char* encryptedText, int key);
+}
+
+#endif // CAESAR_H
