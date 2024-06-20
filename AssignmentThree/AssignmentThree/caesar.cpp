d5192f860e7b22c70b939cd93090f889ae5cddac
gkazimirov396 <grishakazim@gmail.com>
2024-06-20 22:22:59 +0300

    refactor: rewrite ecrypt, decrypt functions

--- AssignmentThree/AssignmentThree/caesar.cpp
+++ AssignmentThree/AssignmentThree/caesar.cpp
@@ -1,38 +1,40 @@
 ﻿#include "caesar.h"
+#include <iostream>
 #include <cstring>
 #include <cctype>
-#include <cstdlib>
 
-char* encrypt(char* rawText, int key) {
-    int len = strlen(rawText);
-    char* encryptedText = (char*)malloc(len + 1);
+extern "C" {
+    __declspec(dllexport) char* encrypt(char* rawText, int key) {
+        int len = strlen(rawText);
+        char* encryptedText = new char[len + 1];
 
-    for (int i = 0; i < len; i++) {
-        if (isalpha(rawText[i])) {
-            char offset = islower(rawText[i]) ? 'a' : 'A';
-            encryptedText[i] = ((rawText[i] - offset + key) % 26) + offset;
+        for (int i = 0; i < len; ++i) {
+            if (isalpha(rawText[i])) {
+                char offset = islower(rawText[i]) ? 'a' : 'A';
+                encryptedText[i] = ((rawText[i] - offset + key) % 26) + offset;
+            }
+            else {
+                encryptedText[i] = rawText[i];
+            }
         }
-        else {
-            encryptedText[i] = rawText[i];
-        }
+        encryptedText[len] = '\0';
+        return encryptedText;
     }
-    encryptedText[len] = '\0';
-    return encryptedText;
-}
 
-char* decrypt(char* encryptedText, int key) {
-    int len = strlen(encryptedText);
-    char* decryptedText = (char*)malloc(len + 1);
+    __declspec(dllexport) char* decrypt(char* encryptedText, int key) {
+        int len = strlen(encryptedText);
+        char* decryptedText = new char[len + 1];
 
-    for (int i = 0; i < len; i++) {
-        if (isalpha(encryptedText[i])) {
-            char offset = islower(encryptedText[i]) ? 'a' : 'A';
-            decryptedText[i] = ((encryptedText[i] - offset - key + 26) % 26) + offset;
+        for (int i = 0; i < len; ++i) {
+            if (isalpha(encryptedText[i])) {
+                char offset = islower(encryptedText[i]) ? 'a' : 'A';
+                decryptedText[i] = ((encryptedText[i] - offset - key + 26) % 26) + offset;
+            }
+            else {
+                decryptedText[i] = encryptedText[i];
+            }
         }
-        else {
-            decryptedText[i] = encryptedText[i];
-        }
+        decryptedText[len] = '\0';
+        return decryptedText;
     }
-    decryptedText[len] = '\0';
-    return decryptedText;
 }
